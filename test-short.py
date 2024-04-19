d3ccd3d16d73264ea100a6854e60ab379f0275d7
sultanak00 <77030655+sultanak00@users.noreply.github.com>
2024-04-18 21:13:56 -0400

    Update test-short.py

--- test-short.py
+++ test-short.py
@@ -4,16 +4,16 @@ import short
 class Test(unittest.TestCase):
 
     def test_x1(self): #it checks the main method
-        self.assertRaises(ValueError, short.process_input("","",1,3))
+        self.assertRaises(None, short.process_input("","",1,3))
     
     def test_y1(self): #it checks the main method
-        self.assertRaises(ValueError, short.process_input(1,"skdk",2,3))
+        self.assertRaises(None, short.process_input(1,"skdk",2,3))
                
     def test_x2(self): #it checks the main method
-        self.assertRaises(ValueError, short.process_input(1,1,"alkdj",5))
+        self.assertRaises(None, short.process_input(1,1,"alkdj",5))
         
     def test_y2(self): #it checks the main method
-        self.assertRaises(ValueError, short.process_input(1,1,5,"a"))
+        self.assertRaises(None, short.process_input(1,1,5,"a"))
     
     def test4(self): #it checks the main method
         self.assertEqual(0, short.shortest_distance(1,1,1,1))
